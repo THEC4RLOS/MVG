1f580f55b3cdebd104f3427363d64a0172943962
car_e <car_e@172.24.19.54>
2016-01-08 15:07:08 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Visor II</title>
+        <link rel="stylesheet" type="text/css" href="./resources/bootstrap/css/bootstrap.min.css">
+        <script src="./resources/angular/angular.js"></script>
+        <script src="./js/app.js"></script>
+    </head>
+    <body ng-app="app" ng-controller="controller">
+    </body>
+</html>
