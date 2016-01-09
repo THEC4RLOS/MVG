4f7853305b2c52bde32b5a993e9ee382a10f6998
hiken13 <mancraft13@hotmail.es>
2016-01-08 18:57:57 -0600

    Creando la conexion con la base de datos y las consultas 08/01/6:56pm

--- Queries/connexion.php
+++ Queries/connexion.php
@@ -7,6 +7,6 @@ class Checker{
         //conexión con la base de datos
         $strconn = "host=$host port=$port dbname=$db user=$usr password=$pass";
         $conn = pg_connect($strconn) or die('{"status":1 , "error":"Error de Conexion con la base de datos"}');
-        return "ok"; // si la conexión es exitosa
+        echo "ok"; // si la conexión es exitosa
     }
 }
