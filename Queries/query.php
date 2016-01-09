d035d60d46ee65992d2a4250341d35e1e9b639ae
hiken13 <mancraft13@hotmail.es>
2016-01-08 18:57:09 -0600

    Creando la conexion con la base de datos y las consultas 08/01/6:56pm

--- Queries/query.php
+++ Queries/query.php
@@ -10,10 +10,23 @@
  */
 class query {
 
-    function conectar($host, $usr, $pass, $port, $db, $type, $name) {
+    /**
+     * Función que decide cual tipo de consulta realizar y prepara el string de
+     * conexión a la base de datos
+     * @param type $host host de la base de datos
+     * @param type $usr usuario de la base de datos
+     * @param type $pass contraseña
+     * @param type $port puierto
+     * @param type $db nombre base de datos
+     * @param type $type tipo de geometría de la capa solicitada
+     * @param type $name nombre de la tabla solicitada
+     * @return string Objeto con los datos necesarios para crear la capa
+     */
+    function conect($host, $usr, $pass, $port, $db, $type, $name) {
         //conexión con la base de datos
         $strconn = "host=$host port=$port dbname=$db user=$usr password=$pass";
         $conn = pg_connect($strconn) or die('{"status":1 , "error":"Error de Conexion con la base de datos"}');
+
         //puntos
         if ($type == "p") {
             return "solicitó puntos";
@@ -28,4 +41,43 @@ class query {
         }
     }
 
+}
+
+function queryForPoints($conn, $type, $name) {
+    $respuesta = array();
+    
+    //obtener las geometrías x,y
+    $query = "select st_asGeoJSON(geom) from hospitales";
+    $result = pg_query($conn, $query) or die('{"status":1 , "error":"Error ern la consulta"}');    
+    while ($row = pg_fetch_row($result)) {
+        $geom = array(
+            "coordenada" => json_decode($row[0]));
+        array_push($respuesta, $geom);
+    }
+    
+    // seleccionar factores mínimos y máximos
+    $query = "select 	min(ST_XMin(geom)) xmin,
+	max(ST_XMax(geom)) xmax,
+	min(ST_yMin(geom)) ymin,
+	max(ST_yMax(geom)) ymax from hoteles";
+
+    $result = pg_query($conn, $query) or die('{"status":1 , "error":"Error ern la consulta"}');
+    $row = pg_fetch_row($result);
+    $dimensiones = array
+        (
+        "xmin" => $row[0],
+        "xmax" => $row[1],
+        "ymin" => $row[2],
+        "ymax" => $row[3]
+    );
+
+    $query = "select srid,type from geometry_Columns where \"f_table_name\"='hoteles'";
+
+    $result = pg_query($conn, $query) or die('{"status":1 , "error":"Error ern la consulta"}');
+    $row = pg_fetch_row($result);
+
+    echo json_encode(array("SRID" => $row[0],
+        "Tipo" => $row[1],
+        "Dimensiones" => $dimensiones,
+        "objs" => $respuesta));
 }
--- Queries/request.php
+++ Queries/request.php
@@ -1,25 +1,29 @@
 <?php
+
 /**
  * Archivo que recibe la petición de realizar la consulta y conexión con la 
  * base de datos, este realiza la petición dados los parámetros de conexión
  * llamando al metodo conexión del archivo Query.php
  */
 require './query.php';
-//http://localhost/MVG/trunk/Queries/request.php?h=localhost&u=postgres&p=12345&port=4321&db=cursoGIS&name=hospitales&type=p
-
-
+require './connexion.php';
+//http://localhost/MVG/trunk/Queries/request.php?h=localhost&u=postgres&p=12345&port=5432&db=cursoGIS&name=hospitales&type=mp
 //datos para la conexión
 $host = $_GET['h']; // host de la base de datos
 $user = $_GET['u']; // nombre de usuario
 $pass = $_GET['p']; // contraseña de usuario
 $port = $_GET['port']; //puerto
 $db = $_GET['db']; // nombre de la base de datos
-
 //datos para realizar la consulta
 $name = $_GET['name']; // nombre de la tabla a consultar
 $type = $_GET['type']; //tipo de geometría de la tabla
 
-$query = new query();
-$layer = $query->conectar($host,$user,$pass,$port,$db,$type,$name);
+if ($name != NULL && $type != NULL) {
+    $query = new query();
+    $layer = $query->conect($host, $user, $pass, $port, $db, $type, $name);
+    echo $layer;
+} else {
+    $checker = new Checker();
+    $conn = $checker->connect($host, $user, $pass, $port, $db);
+}
 
-echo $layer;
