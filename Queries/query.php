0f7517ee7ad918816d5108d54bf11475960e3ff1
Carlos Jimenez Gonzalez <car_ed24@hotmail.es>
2016-01-10 00:34:59 -0600

    commit 2 la matanza de los php (query, coneccion, request)

--- Queries/query.php
+++ Queries/query.php
@@ -1,34 +1,11 @@
 <?php
 
 /**
- * Esta clase establece la conexión con la base de datos y extrae los valores
- *  necesarios para generar la capa.
- * Además se encarga de decidir dados sus parámetros que capa extraer(diferentes
- * geometrías)
+ *  Clase donde se encuentra todas las funciones
  * Retorna un objeto JSON con los valores extraidos de la consulta
  * 
  */
 class query {
-
-    /**
-     * Función que decide cual tipo de consulta realizar y prepara el string de
-     * conexión a la base de datos
-     * @param type $host host de la base de datos
-     * @param type $usr usuario de la base de datos
-     * @param type $pass contraseña
-     * @param type $port puierto
-     * @param type $db nombre base de datos
-     * @param type $type tipo de geometría de la capa solicitada
-     * @param type $name nombre de la tabla solicitada
-     * @return string Objeto con los datos necesarios para crear la capa
-     */
-    function conect($host, $usr, $pass, $port, $db, $type, $name) {
-        //conexión con la base de datos
-        $strconn = "host=$host port=$port dbname=$db user=$usr password=$pass";
-        $conn = pg_connect($strconn) or die('{"status":1 , "error":"Error de Conexion con la base de datos"}');
-
-        return queryForPoints($conn, $name);
-    }
 
     /**
      * Funcion de retorna la informacion de las tablas
@@ -37,39 +14,78 @@ class query {
      */
     function getGeometryColumns($conn) {
         $respuesta = array();
-        $query = "select f_table_name,srid, type from geometry_Columns";
+        $query = "select f_table_name, srid, type, 'false' estado, '[]' puntos, 'false' llamada from geometry_Columns where srid = 5367";
         $result = pg_query($conn, $query) or die('{"status":1 , "error":"Error al obtener datos de las tablas (GeometryColumns)"}');
         while ($row = pg_fetch_row($result)) {
             $geometryColumns = array(
                 "nombre" => $row[0],
                 "srid" => $row[1],
-                "tipo" => $row[2]
+                "tipo" => $row[2],
+                "estado" => json_decode($row[3]),
+                "puntos" => json_decode($row[4]),
+                "llamada" => json_decode($row[5])
             );
             array_push($respuesta, $geometryColumns);
         }
-        return json_encode($respuesta);
+        return $respuesta;
     }
 
-}
+    /**
+     * Funcion que devuelve un JSON con el SRID y las coordenadas de los puntos
+     * necesarios para dibujar las capas compuestas por puntos
+     * @param type $conn string de conexión
+     * @param type $name nombre de la tabla a consultar
+     * @return type JSON con los valores de las coordenadas del geom y el SRID de la
+     * tabla
+     */
+    function getPoints($conn, $name) {
+        $respuesta = array();
 
-/**
- * Funcion que devuelve un JSON con el SRID y las coordenadas de los puntos
- * necesarios para dibujar las capas compuestas por puntos
- * @param type $conn string de conexión
- * @param type $name nombre de la tabla a consultar
- * @return type JSON con los valores de las coordenadas del geom y el SRID de la
- * tabla
- */
-function queryForPoints($conn, $name) {
-    $respuesta = array();
+        //obtener las geometrías x,y
+        $query = "select st_asGeoJSON(geom) from distritos";
+        $result = pg_query($conn, $query) or die('{"status":1 , "error":"Error ern la consulta"}');
+        $row = pg_fetch_all($result);
 
-    //obtener las geometrías x,y
-    $query = "select st_asGeoJSON(geom) from $name";
-    $result = pg_query($conn, $query) or die('{"status":1 , "error":"Error ern la consulta"}');
-    while ($row = pg_fetch_row($result)) {
-        $geom = array(
-            "coordenada" => json_decode($row[0]));
-        array_push($respuesta, $geom);
+        foreach ($row as &$valor) { // puntos
+            //echo strrpos($valor['st_asgeojson'], 'coordinates', 0);
+
+            $d = substr($valor['st_asgeojson'], strrpos($valor['st_asgeojson'], 'coordinates', 0) + strlen('coordinates') + 2, -1);
+
+            //echo ($valor['st_asgeojson']);
+            $as = json_decode($d);
+            //print_r($as[0]);
+            foreach ($as[0] as &$valor2) { // lineas
+
+                foreach ($valor2 as &$valor3) { // poligonos
+                    print_r($valor3);
+                    echo '<br>';
+                    echo '<br>';
+                    echo '<br>';
+                    echo '<br>';
+                    echo '<br>';
+                }
+            }
+        }
+        /* while ($row = pg_fetch_row($result)) {
+          $geom = array(
+          $row[0]);
+          array_push($respuesta, $geom);
+          } */
+        //print_r($row);
     }
-    return $respuesta;
+
+    /**
+     * 
+     * @param type $host
+     * @param type $usr
+     * @param type $pass
+     * @param type $port
+     * @param type $db
+     */
+    function connect($strconn) {
+        //conexión con la base de datos
+        $conn = pg_connect($strconn) or die('{"status":1 , "error":"Error de Conexion con la base de datos"}');
+        echo "ok"; // si la conexión es exitosa
+    }
+
 }
--- Queries/request.php
+++ Queries/request.php
@@ -5,29 +5,31 @@
  * base de datos, este realiza la petición dados los parámetros de conexión
  * llamando al metodo conexión del archivo Query.php
  */
-require './query.php';
-require './connexion.php';
-//http://localhost/MVG/trunk/Queries/request.php?h=localhost&u=postgres&p=12345&port=5432&db=cursoGIS&name=hospitales&type=mp
-//datos para la conexión
-$host = $_GET['h']; // host de la base de datos
-$user = $_GET['u']; // nombre de usuario
-$pass = $_GET['p']; // contraseña de usuario
-$port = $_GET['port']; //puerto
-$db = $_GET['db']; // nombre de la base de datos
-//datos para realizar la consulta
-$name = $_GET['name']; // nombre de la tabla a consultar
-$type = $_GET['type']; //tipo de geometría de la tabla
+require './query2.php';
 
-if ($name != NULL && $type != NULL) {
+$func = $_GET['func'];
+$conn = $_GET['conn'];
+
+if ($func == 'getGeometryColumns') {
+    //http://localhost:8080/mvg/trunk/Queries/request2.php?func=getGeometryColumns&conn=host=localhost%20port=5432%20dbname=cursoGIS%20user=postgres%20password=12345
+    $conn = pg_connect($conn) or die('{"status":1 , "error":"Error de Conexion con la base de datos"}');
     $query = new query();
-    $layer = $query->conect($host, $user, $pass, $port, $db, $type, $name);
-    print_r( $layer);
-    return $layer[0];
+    $geometryColumns = $query->getGeometryColumns($conn);
+    print_r(json_encode($geometryColumns));
+    echo json_encode($geometryColumns);
+    
+} else if ($func == 'getPoints') {
+    //http://localhost:8080/mvg/trunk/Queries/request2.php?func=getPoints&conn=host=localhost%20port=5432%20dbname=cursoGIS%20user=postgres%20password=12345&name=hospitales
+    $name = $_GET['name']; // nombre de la tabla a consultar
+    $conn = pg_connect($conn) or die('{"status":1 , "error":"Error de Conexion con la base de datos"}');
+    $query = new query();
+    $points = $query->getPoints($conn, $name);
+    //print_r($points);
+    //echo $points;
+    
+} else if ($func == 'connect') {
+    //http://localhost:8080/mvg/trunk/Queries/request2.php?func=connect&conn=host=localhost%20port=5432%20dbname=cursoGIS%20user=postgres%20password=12345
+    $state = pg_connect($conn) or die('{"status":1 , "error":"Error de Conexion con la base de datos"}');
+    echo "ok"; // si la conexión es exitosa
+    
 }
-//http://localhost/MVG/trunk/Queries/request.php?h=localhost&u=postgres&p=12345&port=532&db=cursoGIS&name&type
-else {
-    $checker = new Checker();
-    $conn = $checker->connect($host, $user, $pass, $port, $db);
-    echo $conn;
-}
-
